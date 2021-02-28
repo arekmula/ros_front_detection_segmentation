df474ae3c4fcae438971e9c59bf38256286a8214
Arkadiusz Mula <arekmula97@gmail.com>
2021-02-28 08:47:04 +0100

    Added viusualizing instances on image. Updated conda env and README.md

--- README.md
+++ README.md
@@ -2,20 +2,21 @@
 Ros node that's using [MaskRCNN](https://github.com/matterport/Mask_RCNN) and Tensorflow to detect and run segmetation to distinguish rotational fronts from transitional fronts.
 The code for training the network can be found in [another repository](https://github.com/arekmula/mrcnn_instance_segmentation)
 
-The node utilizes conda virtual environment to separate the environment.
+The node utilizes conda virtual environment to separate the environment variables such as Tensorflow version or
+CUDA version.
 ## Dependencies
 - ROS Noetic
 - Anaconda
 
 ## Installation
 - Create conda environment from environment.yml file `conda env create -f environment.yml`
-- Activate environment `conda activate mask_rcnn_ros`
+- Activate environment `conda activate ros_mask_rcnn`
 - Create catkin workspace with Python executable set from conda:
 ```
 source /opt/ros/noetic/setup.bash
 mkdir -p caktin_ws/src
 cd catkin_ws
-catkin_make -DPYTHON_EXECUTABLE=~/anaconda3/envs/mrcnn_ros/bin/python3.6
+catkin_make -DPYTHON_EXECUTABLE=~/anaconda3/envs/ros_mask_rcnn/bin/python3.6
 ```
 - Clone the repository
 ```
--- scripts/front_detection_segmentation_node.py
+++ scripts/front_detection_segmentation_node.py
@@ -1,8 +1,12 @@
 #!usr/bin/env python3.7
 
+# System imports
 import sys
+import threading
 
+# Python packages
 import cv2
+import numpy as np
 
 # ROS imports
 import rospy
@@ -16,26 +20,24 @@ from mask_rcnn import model as modellib
 from mask_rcnn import utils
 from mask_rcnn import visualize
 
-print("Python version")
-print (sys.version)
-
-
 CLASS_NAMES = ["BG", "rot_front", "trans_front"]
 
+# TODO: Try to run with CUDA -> point to cuda tool kit and cudnn from conda environment
 
 class InferenceConfig(coco.FrontHandlerConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
     IMAGE_MIN_DIM = 480
     IMAGE_MAX_DIM = 640
-    DETECTION_MIN_CONFIDENCE = 0.5
+    DETECTION_MIN_CONFIDENCE = 0.95
 
 
 class DetectorSegmentator:
 
     def __init__(self, rgb_image_topic):
-        self.image_sub = rospy.Subscriber(rgb_image_topic, data_class=Image, callback=self.image_callback, queue_size=1,
-                                          buff_size=2**24)
+
+        self.rgb_image_topic = rgb_image_topic
+
         self.cv_bridge = CvBridge()
         config = InferenceConfig()
         config.display()
@@ -51,14 +53,68 @@ class DetectorSegmentator:
         self.class_names = CLASS_NAMES
         self.class_colors = visualize.random_colors(len(self.class_names))
 
-    def image_callback(self, data):
-        try:
-            cv_image = self.cv_bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+        # Last input message and message lock
+        self.last_msg = None
+        self.msg_lock = threading.Lock()
 
-        cv2.imshow("Image", cv_image)
-        cv2.waitKey(1)
+    def run(self):
+        image_sub = rospy.Subscriber(self.rgb_image_topic, data_class=Image, callback=self.image_callback,
+                                     queue_size=1, buff_size=2 ** 24)
+
+        rate = rospy.Rate(100)
+        while not rospy.is_shutdown():
+            # Wait for new image to be acquired
+            if self.msg_lock.acquire(False):
+                msg = self.last_msg
+                self.last_msg = None
+                self.msg_lock.release()
+            else:
+                rate.sleep()
+                continue
+
+            if msg is not None:
+                try:
+                    cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+                except CvBridgeError as e:
+                    print(e)
+
+                # Prediction on single image frame
+                prediction = self.model.detect([cv_image], verbose=0)[0]
+
+                visualized_image = self.visualize_prediction(prediction, cv_image)
+                cv_prediction = np.zeros(shape=visualized_image.shape, dtype=np.uint8)
+                cv2.convertScaleAbs(visualized_image, cv_prediction)
+
+                cv2.imshow("Image", cv_prediction)
+                cv2.waitKey(1)
+
+            rate.sleep()
+
+    def image_callback(self, data):
+        rospy.logdebug("Get input image")
+
+        if self.msg_lock.acquire(False):
+            self.last_msg = data
+            self.msg_lock.release()
+
+    def visualize_prediction(self, prediction, image):
+        from matplotlib.backends.backend_agg import FigureCanvasAgg
+        from matplotlib.figure import Figure
+
+        fig = Figure()
+        canvas = FigureCanvasAgg(fig)
+        axes = fig.gca()
+        visualize.display_instances(image, prediction['rois'], prediction['masks'],
+                                    prediction['class_ids'], self.class_names,
+                                    prediction['scores'], ax=axes,
+                                    )
+        fig.tight_layout()
+        canvas.draw()
+        result = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
+
+        _, _, w, h = fig.bbox.bounds
+        result = result.reshape((int(h), int(w), 3))
+        return result
 
 
 def main(args):
@@ -68,12 +124,8 @@ def main(args):
         print(rgb_image_topic)
 
     detector_segmentator = DetectorSegmentator(rgb_image_topic)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutdown")
-    cv2.destroyAllWindows()
+    detector_segmentator.run()
 
 
 if __name__ == '__main__':
-    main(sys.argv) 
+    main(sys.argv)
